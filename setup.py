188b17b6ca548244811ebc0c68701495d8566099
Robert Collins <robertc@robertcollins.net>
2010-02-20 11:19:41 +1100

    Expand setup.py a little.

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from glob import glob
 from distutils.core import setup
+import os.path
+
+description = file(os.path.join(os.path.dirname(__file__), 'README'), 'rb').read()
 
 setup(
     name='lptools',
@@ -11,7 +14,16 @@ setup(
     author_email='rodney.dawes@canonical.com',
     license='GPLv3',
     description='A collection of tools for developers who use launchpad',
+    long_description=description,
     py_modules=[],
     scripts=glob('bin/*'),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPL3)'
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        ],
     )
 
